ef853cd851001e8f4210ff240daa367df9570611
wuchufan <49216918+wuchufan@users.noreply.github.com>
2020-05-20 20:48:30 -0400

    ready for heroku deploy

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = process.env.PORT || 5000;
 
 
+
 app.use(express.json({extended: false }));
 app.use('/api/email',require('./routes/api/email'));
+
+
+if (process.env.NODE_ENV === 'production'){
+app.use(express.static('client/build'));
+app.get('*',(req,res)=>{
+
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+
+  });
+}
+
 
 
 app.listen(port,()=>{
